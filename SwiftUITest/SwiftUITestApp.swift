4c91f934b9e956f49b9235326a9cc77f24ae512f
mikedev33 <michaelvy365@gmail.com>
2022-07-28 21:07:54 +0300

    Initial Commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Kirill Koban on 28.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
